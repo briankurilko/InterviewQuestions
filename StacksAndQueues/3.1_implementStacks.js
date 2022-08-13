29d9d92eb589402d7019ca033ce48b22fb5cab7e
Brian Kurilko <briankurilko@gmail.com>
2022-08-13 16:49:23 -0400

    Implemented divided (not circular) stack with an array

--- StacksAndQueues/3.1_implementStacks.js
+++ StacksAndQueues/3.1_implementStacks.js
@@ -1,160 +1,104 @@
 class ThreeStacks {
-  constructor() {
-    this.array = [];
-    this.stack1Index = 0;
-    this.stack1CurrentLength = 0;
-    this.stack2Index = 3;
-    this.stack2CurrentLength = 0;
-    this.stack3Index = 7;
-    this.stack3CurrentLength = 0;
-    this.totalArrayLength = 11;
-    this.currentStackLength = 4;
+  constructor(stackSize) {
+    this.numberOfStacks = 3;
+    this.stackCapacity = stackSize;
+    this.values = new Array(this.numberOfStacks * this.stackCapacity);
+    this.sizes = new Array(this.numberOfStacks).fill(0);
   }
 
-  peekStack1() {
-    if (this.stack1CurrentLength === 0) {
-      return null;
-    }
-    return array[this.stack1Index + this.stack1CurrentLength - 1];
-  }
-
-  pushStack1(item) {
-    this.stack1CurrentLength++;
-    if (this.stack1CurrentLength >= this.stack2Index) {
+  push(stackNum, value) {
+    if (this.isFull(stackNum)) {
       this.doubleArraySize();
     }
-    this.array[this.stack1Index + this.stack1CurrentLength - 1] = item;
+    this.sizes[stackNum]++;
+    this.values[this.indexOfTop(stackNum)] = value;
   }
 
-  popStack1() {
-    if (this.stack1CurrentLength === 0) {
-      throw new Error("The stack 1 is currently empty.");
+  pop(stackNum) {
+    if (this.isEmpty(stackNum)) {
+      throw new Error(`Stack ${stackNum} is empty.`);
     }
-    const item = this.array[this.stack1Index + this.stack1CurrentLength - 1];
-    this.array[this.stack1Index + this.stack1CurrentLength - 1] = undefined;
-    this.stack1CurrentLength--;
-    return item;
+    const index = this.indexOfTop(stackNum);
+    const value = this.values[index];
+    this.values[index] = undefined;
+    this.sizes[stackNum]--;
+    return value;
   }
 
-  isEmptyStack1() {
-    return this.stack1CurrentLength === 0;
-  }
-
-  peekStack2() {
-    if (this.stack2CurrentLength === 0) {
-      return null;
+  peek(stackNum) {
+    if (this.isEmpty(stackNum)) {
+      throw new Error(`Stack ${stackNum} is empty.`);
     }
-    return array[this.stack2Index + this.stack2CurrentLength - 1];
+    return this.value[this.indexOfTop(stackNum)];
   }
 
-  pushStack2(item) {
-    this.stack2CurrentLength++;
-    if (this.stack2CurrentLength >= this.stack3Index) {
-      this.doubleArraySize();
-    }
-    this.array[this.stack2Index + this.stack2CurrentLength - 1] = item;
+  isEmpty(stackNum) {
+    return this.sizes[stackNum] === 0;
   }
 
-  popStack2() {
-    if (this.stack2CurrentLength === 0) {
-      throw new Error("The stack 2 is currently empty.");
-    }
-    const item = this.array[this.stack2Index + this.stack2CurrentLength - 1];
-    this.array[this.stack2Index + this.stack2CurrentLength - 1] = undefined;
-    this.stack2CurrentLength--;
-    return item;
+  isFull(stackNum) {
+    return this.sizes[stackNum] === this.stackCapacity;
   }
 
-  isEmptyStack2() {
-    return this.stack2CurrentLength === 0;
-  }
-
-  peekStack3() {
-    if (this.stack3CurrentLength === 0) {
-      return null;
-    }
-    return array[this.stack3Index + this.stack3CurrentLength - 1];
-  }
-
-  pushStack3(item) {
-    this.stack3CurrentLength++;
-    if (this.stack3CurrentLength >= this.totalArrayLength - this.stack3Index) {
-      this.doubleArraySize();
-    }
-    this.array[this.stack3Index + this.stack3CurrentLength - 1] = item;
-  }
-
-  popStack3() {
-    if (this.stack3CurrentLength === 0) {
-      throw new Error("The stack 3 is currently empty.");
-    }
-    const item = this.array[this.stack3Index + this.stack3CurrentLength - 1];
-    this.array[this.stack3Index + this.stack3CurrentLength - 1] = undefined;
-    this.stack3CurrentLength--;
-    return item;
-  }
-
-  isEmptyStack3() {
-    return this.stack3CurrentLength === 0;
+  indexOfTop(stackNum) {
+    const offset = stackNum * this.stackCapacity;
+    const size = this.sizes[stackNum];
+    return offset + size - 1;
   }
 
   doubleArraySize() {
-    const newStack3Index = this.stack3Index + (this.currentStackLength * 2);
-    for (let i = 0; i < this.stack3CurrentLength; ++i) {
-      this.array[i + newStack3Index] = this.array[i + this.stack3Index];
-      this.array[i + this.stack3Index] = undefined;
+    const newStackCapacity = this.stackCapacity * 2;
+    const newValuesArray = new Array(newStackCapacity * this.numberOfStacks);
+    let stackToCopy = this.numberOfStacks - 1;
+    while (stackToCopy >= 0) {
+      let offset = stackToCopy * this.stackCapacity;
+      let newOffset = stackToCopy * newStackCapacity;
+      let currentStackSize = this.sizes[stackToCopy];
+      while (currentStackSize - 1 >= 0) {
+        newValuesArray[newOffset + currentStackSize - 1] =
+          this.values[offset + currentStackSize - 1];
+        --currentStackSize;
+      }
+      --stackToCopy;
     }
-    this.stack3Index = newStack3Index;
-
-    const newStack2Index = this.stack2Index + this.currentStackLength;
-    for (let i = 0; i < this.stack2CurrentLength; ++i) {
-      this.array[i + newStack2Index] = this.array[i + this.stack2Index];
-      this.array[i + this.stack2Index] = undefined;
-    }
-    this.stack2Index = newStack2Index;
-
-    this.currentStackLength = this.currentStackLength * 2;
-    this.totalArrayLength = this.totalArrayLength * 2;
+    this.stackCapacity = newStackCapacity;
+    this.values = newValuesArray;
   }
 
   printArray() {
-      console.log(this.array);
-      console.log(this.totalArrayLength);
-      console.log(this.stack2Index);
-      console.log(this.stack3Index);
+    console.log(this.values);
+    console.log(this.sizes);
+    console.log(this.values.length);
   }
-
 }
 
-const myStack = new ThreeStacks();
 
-myStack.pushStack1(1);
-myStack.pushStack1(2);
-myStack.pushStack3(7);
-myStack.pushStack3(9);
-myStack.pushStack3(10);
-myStack.pushStack3(10);
-myStack.pushStack3(10);
-myStack.pushStack3(10);
-myStack.pushStack3(12);
-myStack.pushStack3(13);
-myStack.pushStack2(11);
-myStack.pushStack2(12);
-myStack.pushStack2(13);
+const myStack = new ThreeStacks(1);
 
-console.log(myStack.popStack1());
-console.log(myStack.popStack1());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack3());
-console.log(myStack.popStack2());
-console.log(myStack.popStack2());
-console.log(myStack.popStack2());
-myStack.printArray();
+myStack.push(0, 1);
+myStack.push(0, 2);
+myStack.push(2, 7);
+myStack.push(2, 9);
+myStack.push(2, 10);
+myStack.push(2, 10);
+myStack.push(2, 10);
+myStack.push(2, 10);
+myStack.push(2, 12);
+myStack.push(2, 13);
+myStack.push(1, 11);
+myStack.push(1, 12);
+myStack.push(1, 13);
 
-myStack.popStack3();
+console.log(myStack.pop(0)); // should be 2
+console.log(myStack.pop(0)); // should be 1
+console.log(myStack.pop(2)); // should be 13
+console.log(myStack.pop(2)); // should be 12
+console.log(myStack.pop(2)); // should be 10
+console.log(myStack.pop(2)); // should be 10
+console.log(myStack.pop(2)); // should be 10
+console.log(myStack.pop(2)); // should be 10
+console.log(myStack.pop(2)); // should be 9
+console.log(myStack.pop(2)); // should be 7
+console.log(myStack.pop(1)); // should be 13
+console.log(myStack.pop(1)); // should be 12
+console.log(myStack.pop(1)); // should be 11
