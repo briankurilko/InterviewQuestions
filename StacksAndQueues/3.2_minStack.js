9e629d2d3f0acacd5d1cbf0234ca92e732bebeab
Brian Kurilko <briankurilko@gmail.com>
2022-08-17 22:57:49 -0400

    Implemented minStack

--- StacksAndQueues/3.2_minStack.js
+++ StacksAndQueues/3.2_minStack.js
@@ -1,20 +1,58 @@
 class MinStack {
   constructor() {
     this.stack = [];
-    this.minStack = [];
+    this.min = null;
   }
 
   push(value) {
-    if (this.minStack.length === 0 || this.minStack[this.minStack.length - 1] < value) {
-      this.minStack = 
+    let previousMin = this.min;
+    if (
+      this.min === null ||
+      value < this.min
+    ) {
+      this.min = value;
     }
-    this.stack.push(value);
+    this.stack.push({ value, previousMin });
   }
 
   pop() {
-    const value = this.stack.pop();
-    if (this.max && this.max === value) {
-        this.max = this.previousMax;
+    if (this.isEmpty()) {
+      return null;
     }
+    const { value, previousMin } = this.stack.pop();
+    this.min = previousMin;
+    return value;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin() {
+    return this.min;
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
   }
 }
+
+const minStack = new MinStack();
+
+minStack.push(13);
+minStack.push(123);
+minStack.push(123);
+minStack.push(12);
+minStack.push(1212354);
+minStack.push(11234234324);
+
+console.log(minStack.getMin());
+minStack.pop();
+minStack.pop();
+minStack.pop();
+minStack.pop();
+
+console.log(minStack.getMin());
