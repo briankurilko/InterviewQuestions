6a00825534e1b73ad6f10f9d4595dfee0c411240
Brian Kurilko <briankurilko@gmail.com>
2022-09-25 15:21:22 -0400

    Implemented a slightly more optimized sorted stack implementation

--- StacksAndQueues/3.5_sortedStack.js
+++ StacksAndQueues/3.5_sortedStack.js
@@ -1,4 +1,5 @@
 class SortedStackUnoptimized {
+  // O(1) push, O(N) peek and pop. Doesn't work with repeated elements at the moment.
   constructor() {
     this.stack = [];
     this.tempStack = [];
@@ -47,33 +48,90 @@ class SortedStackUnoptimized {
 }
 
 class SortedStackOptimized {
+  // O(1) pop and peek, O(n^2) worst case push, but possibly O(1) push. Even if we have to sort stacks, 
+  // we don't necessarily sort the entire stack unless we get the biggest element we've ever received, so hopefully we
+  // don't usually loop through the entire array at once.
   constructor() {
     this.stack = [];
+    this.sortedStack = [];
   }
 
   push(value) {
-    this.stack.push(value);
+    if (
+      this.sortedStack.length === 0 ||
+      value < this.sortedStack[this.sortedStack.length - 1]
+    ) {
+      this.sortedStack.push({ value });
+    } else {
+      this.sortStacks(value);
+    }
   }
 
-  sortStacks() {
-    const tempStack = [];
-    if (this.stack.length !== 0) {
-      while (this.sortedStack.length !== 0) {
-        this.stack.push(this.sortedStack.pop());
+  sortStacks(newestValue) {
+    let newValuePushedIntoSortedStack = false;
+    while (this.sortedStack.length !== 0) {
+      if (newestValue <= this.sortedStack[this.sortedStack.length - 1].value) {
+        this.sortedStack.push({ value: newestValue });
+        newValuePushedIntoSortedStack = true;
+        break;
       }
-      while (this.stack.length != 0) {
-          
-      }
+      this.stack.push(this.sortedStack.pop());
     }
+    if (!newValuePushedIntoSortedStack) {
+      console.log("hooray");
+      this.stack.push({ value: newestValue });
+    }
+    while (this.stack.length !== 0) {
+      const maxElement = this.findAndPopMaximumElement();
+      this.sortedStack.push(maxElement);
+    }
+  }
+
+  findAndPopMaximumElement() {
+    let maxElement = null;
+    let count = 0;
+    while (this.stack.length !== 0) {
+      const element = this.stack.pop();
+      if (maxElement === null || maxElement.value < element.value) {
+        maxElement = element;
+      }
+      this.sortedStack.push(element);
+      ++count;
+    }
+    while (count > 0) {
+      const currentElement = this.sortedStack.pop();
+      if (currentElement !== maxElement) {
+        this.stack.push(currentElement);
+      }
+      --count;
+    }
+    return maxElement;
+  }
+
+  peek() {
+    return this.sortedStack[this.sortedStack.length - 1].value;
+  }
+
+  pop() {
+    return this.sortedStack.pop().value;
+  }
+
+  printStacks() {
+    console.log(this.sortedStack);
   }
 }
 
-const myStack = new SortedStackUnoptimized();
+const myStack = new SortedStackOptimized();
 
 myStack.push(4);
 myStack.push(2);
+myStack.push(2);
+myStack.push(3);
 myStack.push(100);
+myStack.push(120);
 
-console.log(myStack.peek());
-console.log(myStack.pop());
-console.log(myStack.peek());
+console.log(myStack.pop()); // 2
+console.log(myStack.pop()); // 2
+console.log(myStack.pop()); // 3
+console.log(myStack.pop()); // 4
+console.log(myStack.pop()); // 100
