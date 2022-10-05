33ff758b56fce2c47eff2ce5cdaef9ab2eab74e0
Brian Kurilko <briankurilko@gmail.com>
2022-10-05 17:18:41 -0400

    got minimal depth binary search tree implemented

---
+++ TreesAndGraphsQuestions/4.2_minimalTree.js
@@ -0,0 +1,67 @@
+const sortedArray = [];
+
+sortedArray.push(10);
+sortedArray.push(5);
+sortedArray.push(130);
+sortedArray.push(104);
+sortedArray.push(1123);
+sortedArray.push(1234);
+sortedArray.push(1);
+sortedArray.push(2);
+sortedArray.push(1124);
+
+sortedArray.sort((a, b) => a - b);
+
+function createBinarySearchTreeFromSortedArray(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  const middle = findMiddleElementIndex(array);
+  return {
+    data: array[middle],
+    right: createBinarySearchTreeFromSortedArray(array.slice(middle + 1)),
+    left: createBinarySearchTreeFromSortedArray(array.slice(0, middle)),
+  };
+}
+
+// this has better space complexity - slice creates a copy of the array.
+function createBinarySearchTreeFromSortedArray_noSlice(
+  array,
+  start = 0,
+  end = array.length - 1
+) {
+  if (end < start) {
+    return null;
+  }
+  const middle = Math.floor((end + start) / 2);
+  return {
+    data: array[middle],
+    right: createBinarySearchTreeFromSortedArray_noSlice(
+      array,
+      middle + 1,
+      end
+    ),
+    left: createBinarySearchTreeFromSortedArray_noSlice(
+      array,
+      start,
+      middle - 1
+    ),
+  };
+}
+
+function findMiddleElementIndex(array) {
+  return Math.floor((array.length - 1) / 2);
+}
+
+console.log(
+  JSON.stringify(createBinarySearchTreeFromSortedArray(sortedArray)) ===
+    JSON.stringify(createBinarySearchTreeFromSortedArray_noSlice(sortedArray))
+);
+
+console.log(
+  JSON.stringify(createBinarySearchTreeFromSortedArray_noSlice(sortedArray))
+);
+
+console.log(JSON.stringify(createBinarySearchTreeFromSortedArray(sortedArray)));
+
+console.log(sortedArray.length);
--- maxNumberInMatrix.js
+++ maxNumberInMatrix.js
@@ -27,3 +27,27 @@ const testArray = [
 ];
 
 findMaxInMatrix(array, [0, 0]);
+
+// first convert matrix to tree I guess? Or adjacency list? How would you do that...
+// 9: 3, 10.
+// 3: 4, 4.
+// 4: 5
+// 10: 4, 5
+// 4: 5, 3
+// 5: 2
+// 5: 3
+// 3: 2
+// 2: []
+function convertToAdjacencyList(matrix) {
+  const adjacencyList = [];
+  // maybe loop through matrix backwards?
+  for (let i = matrix.length - 1; i >= 0; --i) {
+    for (let j = matrix[0].length - 1; j >= 0; ++j) {
+      let newNode = { value: matrix[i][j], adjacent: [] };
+      if (matrix[i][j + 1] !== undefined) {
+        newNode.adjacent.push(new Node()); // idk man.
+      }
+      adjacencyList.unshift(newNode);
+    }
+  }
+}
