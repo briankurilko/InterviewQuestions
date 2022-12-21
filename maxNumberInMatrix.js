b2e2811fbc0e31c70f1f8c40f20a57c248bb474b
Brian Kurilko <brian.kurilko@cengage.com>
2022-12-20 22:58:15 -0500

    Added more stuff

--- maxNumberInMatrix.js
+++ maxNumberInMatrix.js
@@ -32,10 +32,10 @@ function findMaxInMatrixBruteForceHelper(
 
 // This is probably around O(2^(number of paths)) time complexity, and O(number of paths) space complexity. Idk how to find the number of paths.
 function findMaxInMatrixBruteForce(matrix) {
-  return Math.max(...findMaxInMatrixBruteForceHelper(matrix));
+  return getMax(findMaxInMatrixBruteForceHelper(matrix));
 }
 
-// Alright, solved with memoizing! O(n) time and O(n) space, where n is the number of cells in the matrix.
+// Alright, solved with memoizing! O(number of paths) time complexity/space complexity. Could probably save memory still...
 function findMaxInMatrixMemoizedHelper(
   matrix,
   coordinate = [0, 0],
@@ -89,18 +89,67 @@ function findMaxInMatrixMemoizedHelper(
 
 function findMaxInMatrixMemoized(matrix) {
   const result = findMaxInMatrixMemoizedHelper(matrix);
-  return Math.max(...result);
+  return getMax(result);
 }
+
+function getMax(arr) {
+  let len = arr.length;
+  let max = -Infinity;
+
+  while (len--) {
+    max = arr[len] > max ? arr[len] : max;
+  }
+  return max;
+}
+
+// I think the best way to do this is to go through the array and go backwards, and only memoize the max length path, maybe? Idk.
 
 // I think 30 is the highest path here?
 const testArray = [
-  [9, 3, 4],
-  [10, 4, 5],
-  [5, 3, 2],
+  [
+    0, 1, 2, 3, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4,
+  ],
+  [
+    5, 6, 7, 8, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+  ],
+  [
+    10, 11, 12, 13, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 14,
+  ],
+  [
+    15, 16, 17, 18, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 19,
+  ],
+  [
+    20, 21, 22, 23, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 24,
+  ],
+  [
+    25, 26, 27, 28, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 29,
+  ],
+  [
+    30, 31, 32, 33, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 34,
+  ],
+  [
+    35, 36, 37, 38, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 39,
+  ],
+  [
+    40, 41, 42, 43, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 44,
+  ],
+  [
+    45, 46, 47, 48, 13, 43, 53, 123, 34243, 5432, 43221, 231541324543, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 49,
+  ],
 ];
 
+// console.log(findMaxInMatrixMemoized(testArray));
 console.log(findMaxInMatrixBruteForce(testArray));
-console.log(findMaxInMatrixMemoized(testArray));
 
 // https://www.geeksforgeeks.org/maximum-sum-path-in-a-matrix-from-top-left-to-bottom-right/
 // https://workat.tech/problem-solving/approach/mpsm/max-path-sum-matrix
