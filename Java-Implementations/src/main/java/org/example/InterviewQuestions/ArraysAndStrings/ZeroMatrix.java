1f64cdf404966a79fc78952e3a862434677a1585
Brian Kurilko <briankurilko@gmail.com>
2022-12-30 21:48:36 -0500

    Fully implemented zero matrix

--- Java-Implementations/src/main/java/org/example/InterviewQuestions/ArraysAndStrings/ZeroMatrix.java
+++ Java-Implementations/src/main/java/org/example/InterviewQuestions/ArraysAndStrings/ZeroMatrix.java
@@ -9,9 +9,7 @@ import java.util.Set;
 // column are set to O.
 public class ZeroMatrix {
 
-    // You remember, there's a way to do this is O(N * M) time and O(N * M) space,
-    // but there's also a way to do this in O(N * M) time and O(1) space if we edit it in place.
-    // Because there's no constraint on the numbers that can be in the matrix, I'll do the set implementation first.
+    // O(N * M) time, O(N * M) space
     public static void zeroMatrixSetImpl(int[][] matrix) {
         Set<Integer> rowsWithZeros = new HashSet<>();
         Set<Integer> colsWithZeros = new HashSet<>();
@@ -35,6 +33,72 @@ public class ZeroMatrix {
             for (int i = 0; i < matrix.length; ++i) {
                 matrix[i][col] = 0;
             }
+        }
+    }
+
+    // O(n * m) time, O(1) space.
+    // Note that this assumes that the matrix will never have -1 in it. If we were using an Integer matrix, we could
+    // use null instead, but since we're not, we'll just use null.
+    public static void zeroMatrixInPlace(int[][] matrix) {
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[0].length; ++j) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) {
+                        firstRowHasZero = true;
+                    }
+                    if (j == 0) {
+                        firstColHasZero = true;
+                    }
+                    matrix[0][j] = -1;
+                    matrix[i][0] = -1;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; ++i) {
+            if (matrix[i][0] == -1) {
+                nullifyRow(matrix, i);
+            }
+        }
+
+        for (int j = 1; j < matrix[0].length; ++j) {
+            if (matrix[0][j] == -1) {
+                nullifyCol(matrix, j);
+            }
+        }
+
+        if (firstRowHasZero) {
+            nullifyRow(matrix, 0);
+        }
+
+        if (firstColHasZero) {
+            nullifyCol(matrix, 0);
+        }
+
+        for (int i = 0; i < matrix.length; ++i) {
+            if (matrix[i][0] == -1) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        for (int j = 0; j < matrix[0].length; ++j) {
+            if (matrix[0][j] == -1) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    private static void nullifyRow(int[][] matrix, int row) {
+        for (int j = 1; j < matrix[0].length; ++j) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    private static void nullifyCol(int[][] matrix, int column) {
+        for (int i = 1; i < matrix.length; ++i) {
+            matrix[i][column] = 0;
         }
     }
 }
--- Java-Implementations/src/test/java/org/example/InterviewQuestions/ArraysAndStrings/ZeroMatrixTest.java
+++ Java-Implementations/src/test/java/org/example/InterviewQuestions/ArraysAndStrings/ZeroMatrixTest.java
@@ -11,16 +11,104 @@ class ZeroMatrixTest {
     @Test
     void zeroMatrixSetImpl() {
         int[][] matrix = {
-                {5, 1, 2, 3, 4, 5},
+                {5, 1, 2, 3, 4, 0},
                 {8, 9, 10, 4, 1, 2},
                 {5, 6, 7, 8, 9, 10},
                 {2, 3, 4, 5, 6, 7},
-                {10, 0, 1, 2, 3, 4},
+                {10, 3, 1, 2, 3, 4},
+                {7, 8, 9, 10, 0, 1},
+                {0, 5, 6, 7, 8, 9},
+        };
+
+        int[][] expected = {
+                {0, 0, 0, 0, 0, 0},
+                {0, 9, 10, 4, 0, 0},
+                {0, 6, 7, 8, 0, 0},
+                {0, 3, 4, 5, 0, 0},
+                {0, 3, 1, 2, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}
+        };
+
+        ZeroMatrix.zeroMatrixSetImpl(matrix);
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(matrix));
+    }
+
+    @Test
+    void zeroMatrixInPlace() {
+        int[][] matrix = {
+                {5, 1, 2, 3, 4, 0},
+                {8, 9, 10, 4, 1, 2},
+                {5, 6, 7, 8, 9, 10},
+                {2, 3, 4, 5, 6, 7},
+                {10, 3, 1, 2, 3, 4},
+                {7, 8, 9, 10, 0, 1},
+                {0, 5, 6, 7, 8, 9},
+        };
+
+        int[][] expected = {
+                {0, 0, 0, 0, 0, 0},
+                {0, 9, 10, 4, 0, 0},
+                {0, 6, 7, 8, 0, 0},
+                {0, 3, 4, 5, 0, 0},
+                {0, 3, 1, 2, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}
+        };
+
+        ZeroMatrix.zeroMatrixInPlace(matrix);
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(matrix));
+    }
+
+    @Test
+    void zeroMatrixInPlace_NoSideZeros() {
+        int[][] matrix = {
+                {5, 1, 2, 3, 4, 3},
+                {8, 9, 10, 4, 1, 2},
+                {5, 6, 7, 8, 9, 10},
+                {2, 3, 4, 5, 6, 7},
+                {10, 3, 1, 2, 3, 4},
                 {7, 8, 9, 10, 0, 1},
                 {4, 5, 6, 7, 8, 9},
         };
 
+        int[][] expected = {
+                {5, 1, 2, 3, 0, 3},
+                {8, 9, 10, 4, 0, 2},
+                {5, 6, 7, 8, 0, 10},
+                {2, 3, 4, 5, 0, 7},
+                {10, 3, 1, 2, 0, 4},
+                {0, 0, 0, 0, 0, 0},
+                {4, 5, 6, 7, 0, 9}
+        };
+
+        ZeroMatrix.zeroMatrixInPlace(matrix);
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(matrix));
+    }
+
+    @Test
+    void zeroMatrixSetImpl_NoSideZeros() {
+        int[][] matrix = {
+                {5, 1, 2, 3, 4, 3},
+                {8, 9, 10, 4, 1, 2},
+                {5, 6, 7, 8, 9, 10},
+                {2, 3, 4, 5, 6, 7},
+                {10, 3, 1, 2, 3, 4},
+                {7, 8, 9, 10, 0, 1},
+                {4, 5, 6, 7, 8, 9},
+        };
+
+        int[][] expected = {
+                {5, 1, 2, 3, 0, 3},
+                {8, 9, 10, 4, 0, 2},
+                {5, 6, 7, 8, 0, 10},
+                {2, 3, 4, 5, 0, 7},
+                {10, 3, 1, 2, 0, 4},
+                {0, 0, 0, 0, 0, 0},
+                {4, 5, 6, 7, 0, 9}
+        };
+
         ZeroMatrix.zeroMatrixSetImpl(matrix);
-        System.out.println(Arrays.deepToString(matrix));
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(matrix));
     }
 }
