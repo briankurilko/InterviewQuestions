016a7c9e5007ffeb6bc72d5b233191cbd63c132b
Brian Kurilko <brian.kurilko@cengage.com>
2023-01-06 16:35:33 -0500

    Completed minimal number of moves question

--- JavaScript-Implementations/MiscellaneousLeetCodes/MinimalNumberOfMovesToPalindrome.js
+++ JavaScript-Implementations/MiscellaneousLeetCodes/MinimalNumberOfMovesToPalindrome.js
@@ -5,3 +5,44 @@
 // Return the minimum number of moves needed to make s a palindrome.
 // Note that the input will be generated such that s can always be converted to a palindrome.
 
+function stepsToPalindrome(charArray) {
+  let left = 0;
+  let right = charArray.length - 1;
+  let k = right;
+  let steps = 0;
+  let middleIndex = null;
+
+  while (left < right) {
+    if (charArray[left] !== charArray[right]) {
+      k = right;
+      while (k !== left) {
+        if (charArray[k] == charArray[left]) {
+          break;
+        }
+        --k;
+      }
+      if (k === left) {
+        middleIndex = k;
+        left++;
+        continue;
+      } else {
+        while (k < right) {
+          let temp = charArray[k + 1];
+          charArray[k + 1] = charArray[k];
+          charArray[k] = temp;
+          ++steps;
+          ++k;
+        }
+      }
+    }
+    ++left;
+    --right;
+  }
+  if (middleIndex !== null) {
+    steps += Math.floor(charArray.length / 2) - middleIndex;
+  }
+  return steps;
+}
+
+console.log(stepsToPalindrome("accabb".split("")));
+console.log(stepsToPalindrome("scpcyxprxxsjyjrww".split("")));
