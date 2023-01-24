c6f282b7a5e30d7931b41d5bca8d5359788ce9bd
Brian Kurilko <brian.kurilko@cengage.com>
2023-01-24 16:19:59 -0500

    Implemented 2d prefix sum

--- Java-Implementations/src/main/java/org/example/InterviewQuestions/MiscellaneousLeetcodes/NumMatrix.java
+++ Java-Implementations/src/main/java/org/example/InterviewQuestions/MiscellaneousLeetcodes/NumMatrix.java
@@ -3,21 +3,18 @@ package org.example.InterviewQuestions.MiscellaneousLeetcodes;
 // https://leetcode.com/problems/range-sum-query-2d-immutable/
 public class NumMatrix {
     private final int[][] prefixSum2D;
+
     public NumMatrix(int[][] matrix) {
         prefixSum2D = new int[matrix.length + 1][matrix[0].length + 1];
         // alright. 2D prefix sum. Wonder how you do that.
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (i == 0 || j == 0) {
-                    prefixSum2D[i][j] = 0;
-                } else {
-                    prefixSum2D[i][j] = prefixSum2D[i - 1][j] + prefixSum2D[i][j - 1] - prefixSum2D[i - 1][j - 1] + matrix[i][j];
-                }
+        for (int i = 1; i < matrix.length + 1; ++i) {
+            for (int j = 1; j < matrix[0].length + 1; ++j) {
+                prefixSum2D[i][j] = prefixSum2D[i - 1][j] + prefixSum2D[i][j - 1] - prefixSum2D[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return prefixSum2D[row2][col2] - prefixSum2D[row1 - 1][col2] - prefixSum2D[row2][col1 - 1] + prefixSum2D[row1 - 1][col1 - 1];
+        return prefixSum2D[row2 + 1][col2 + 1] - prefixSum2D[row1][col2 + 1] - prefixSum2D[row2 + 1][col1] + prefixSum2D[row1][col1];
     }
 }
--- Java-Implementations/src/test/java/org/example/InterviewQuestions/MiscellaneousLeetcodes/NumMatrixTest.java
+++ Java-Implementations/src/test/java/org/example/InterviewQuestions/MiscellaneousLeetcodes/NumMatrixTest.java
@@ -7,8 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class NumMatrixTest {
 
     @Test
-    void sumRegion() {
+    void sumRegionSmallMatrix() {
         NumMatrix numMatrix = new NumMatrix(new int[][]{{-1}});
         assertEquals(-1, numMatrix.sumRegion(0, 0, 0, 0));
+    }
+
+    @Test
+    void sumRegion() {
+        NumMatrix numMatrix = new NumMatrix(new int[][]{
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}
+        });
+        assertEquals(8, numMatrix.sumRegion(2, 1, 4, 3));
+        assertEquals(11, numMatrix.sumRegion(1, 1, 2, 2));
+        assertEquals(12, numMatrix.sumRegion(1, 2, 2, 4));
     }
 }
